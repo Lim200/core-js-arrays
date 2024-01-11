1dacca237036daf6008b55b82bc6a18926788c18
Lim200 <112889518+Lim200@users.noreply.github.com>
2024-01-11 17:06:39 +0300

    feat: 6 passing 43 pending

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -67,8 +67,10 @@ function sumArrays(arr1, arr2) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  let result = 0;
+  result = arr.indexOf(value);
+  return result;
 }
 
 /**
